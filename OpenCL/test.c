25e2ce07229fe97f7e1d786f92d5db7a4247c774
linkenzone <linkenzone@163.com>
2023-07-21 10:58:17 +0800

    更新CL的教学代码

---
+++ OpenCL/test.c
@@ -0,0 +1,25 @@
+/*
+ * @Author: linkenzone
+ * @Date: 2023-07-21 10:05:20
+ * @Descripttion: Do not edit
+ */
+// C standard includes
+#include <stdio.h>
+
+// OpenCL includes
+#include <CL/cl.h>
+
+int main()
+{
+    cl_int CL_err = CL_SUCCESS;
+    cl_uint numPlatforms = 0;
+
+    CL_err = clGetPlatformIDs( 0, NULL, &numPlatforms );
+
+    if (CL_err == CL_SUCCESS)
+        printf_s("%u platform(s) found\n", numPlatforms);
+    else
+        printf_s("clGetPlatformIDs(%i)\n", CL_err);
+
+    return 0;
+}
