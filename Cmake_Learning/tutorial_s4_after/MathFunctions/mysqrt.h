942ccc787cae1a8acf4994fdb79e27b1b101465b
linkenzone <linkenzone@163.com>
2023-07-25 09:50:32 +0800

    更新到s4

--- Cmake_Learning/tutorial_s4_after/MathFunctions/mysqrt.h
+++ Cmake_Learning/tutorial_s4_after/MathFunctions/mysqrt.h
@@ -1,11 +1,15 @@
-/*
+﻿/*
  * @Author: linkenzone
  * @Date: 2023-07-24 15:43:50
  * @Descripttion: Do not edit
  */
+
 #pragma once
 
 namespace mathfunctions
 {
-    double sqrt(double x);
+    namespace detail
+    {
+        double mysqrt(double x);
+    }
 }
