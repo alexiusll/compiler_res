4e1cdd5f3c62e587789e59d2b32fbe0835e09d1f
linkenzone <linkenzone@163.com>
2023-09-15 17:11:45 +0800

    更新

--- OpenCL/compiler.bat
+++ OpenCL/compiler.bat
@@ -1,1 +1,5 @@
-gcc test.c -I C:\env\OpenCL\include -L C:\env\OpenCL\lib -lOpenCL -o test.exe
+:: gcc test.c -I C:\env\OpenCL\include -L C:\env\OpenCL\lib -lOpenCL -o test.exe
+
+:: gcc test.c -I C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\include -L C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.2\lib\Win32 -lOpenCL -o test.exe
+
+gcc test.c -I C:\env\CUDA\OpenCL\include -L C:\env\CUDA\OpenCL\lib -lOpenCL -o test.exe
--- OpenCL/my/first/compiler.bat
+++ OpenCL/my/first/compiler.bat
@@ -1,1 +1,3 @@
-gcc main.c -I C:\env\OpenCL\include -L C:\env\OpenCL\lib -lOpenCL -o main.exe
+:: gcc main.c -I C:\env\OpenCL\include -L C:\env\OpenCL\lib -lOpenCL -o main.exe
+
+gcc main.c -I C:\env\CUDA\OpenCL\include -L C:\env\CUDA\OpenCL\lib -lOpenCL -o main.exe
