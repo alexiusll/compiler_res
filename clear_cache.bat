a88ae106bedb434eb7fec88a981180fa2f38b02a
wzh <linkenzone@163.com>
2024-09-05 19:44:10 +0800

    更新

--- clear_cache.bat
+++ clear_cache.bat
@@ -1,9 +1,14 @@
+:: 清理临时文件
+chcp 65001
+
 @echo off
-echo clean...
+echo clean
+
 del %~dp0*.exe /q /s
 del %~dp0tempCodeRunnerFile.c /q /s
 del %~dp0a.out /q /s
 del %~dp0*.o /q /s
 echo end...press
-pause >nul
-exit
+
+:: pause >nul
+:: exit
