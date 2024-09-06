b431698686154935f4b47526280072b227f90d2b
wzh <linkenzone@163.com>
2024-09-06 15:11:02 +0800

    更新

--- source/cpp/main.cpp
+++ source/cpp/main.cpp
@@ -4,9 +4,24 @@
  * @Descripttion: Do not edit
  */
 
-#include <iostream>
+// #include <iostream>
+
+int __attribute__((noinline)) help(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 {
-    std::cout << "233";
+    int a = 1;
+    int b = 2;
+
+    if(help(a,b) > 4)
+    {
+        b = 0;
+    }
+    else
+    {
+        b = 1;
+    }
 }
